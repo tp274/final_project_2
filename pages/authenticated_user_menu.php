32daeef4c87ee328e5e0ad20f36010e250975598
Trupti <tp274@njit.edu>
2017-12-14 11:40:01 -0500

    Changes to remove ownerid

---
+++ pages/authenticated_user_menu.php
@@ -0,0 +1,45 @@
+<html>
+<body>
+<!-- Fixed navbar
+<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Fixed navbar</a>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#"> <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+        <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+-->
+
+
+<div class="card text-center">
+    <div class="card-header">
+        <ul class="nav nav-pills card-header-pills">
+            <li class="nav-item">
+                <a class="nav-link active" href="index.php?page=accounts&action=all">show all accounts</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?page=tasks&action=all">Show all task</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="index.php?page=accounts&action=logout">Logout</a>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
