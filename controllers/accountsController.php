f3f854c7bad9d667edd07a350224a1e71798e577
Trupti <tp274@njit.edu>
2017-12-14 11:49:08 -0500

    Logout function

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -97,7 +97,7 @@ class accountsController extends http\controller
         $user->birthday = $_POST['birthday'];
         $user->gender = $_POST['gender'];
         $user->save();
-        header("Location: index.php?page=accounts&action=all");
+        header("Location: index.php");
 
     }
 
@@ -120,12 +120,12 @@ class accountsController extends http\controller
 
         $user = accounts::findUserbyEmail($_REQUEST['email']);
 
-
         if ($user == FALSE) {
             echo 'user not found';
         } else {
-
+           // var_dump($user->checkPassword($_POST['password']));
             if($user->checkPassword($_POST['password']) == TRUE) {
+
 
                 echo 'login';
 
@@ -133,7 +133,8 @@ class accountsController extends http\controller
                 $_SESSION["userID"] = $user->id;
 
                 //forward the user to the show all todos page
-                print_r($_SESSION);
+               // print_r($_SESSION);
+                header("Location: index.php?page=tasks&action=all");
             } else {
                 echo 'password does not match';
             }
@@ -143,6 +144,12 @@ class accountsController extends http\controller
 
 
 
+    }
+
+    public static function logout(){
+        session_start();
+        unset($_SESSION["userID"]);
+        header("Location: index.php");
     }
 
 }
