fabf718ee904587281fb647368412f7bb5adae06
Trupti <tp274@njit.edu>
2017-12-14 11:41:40 -0500

    Included header and footer

--- pages/all_tasks.php
+++ pages/all_tasks.php
@@ -1,31 +1,19 @@
-<!doctype html>
-
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-
 <?php
+include 'header.php';
 //this is how you print something
 
-print utility\htmlTable::genarateTableFromMultiArray($data);
-
+if($data == false){
+    echo '<p> You have no tasks </p>';
+}else{
+    print utility\htmlTable::genarateTableFromMultiArray($data);
+}
 
 ?>
-
-
-<script src="js/scripts.js"></script>
+<html>
+<body>
+<form action="index.php?page=tasks&action=create" method="post" id="form1">
+    <button type="submit" form="form1" value="create">Create Task</button>
+</form>
+<!--<h1><a href="index.php?page=tasks&action=create">create task</a></h1> -->
 </body>
 </html>
