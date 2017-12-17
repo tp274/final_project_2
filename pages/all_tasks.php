96d4ffab9d60b2145ff7399b6b50a00270570364
Trupti <tp274@njit.edu>
2017-12-17 13:54:43 -0500

    changes

--- pages/all_tasks.php
+++ pages/all_tasks.php
@@ -1,15 +1,18 @@
 <?php
 include 'header.php';
-//this is how you print something
+?>
 
+<?php
 if($data == false){
     echo '<p> You have no tasks. </p>';
 }else{
     print utility\htmlTable::genarateTableFromMultiArray($data);
 }
-
 ?>
+<div class ="card">
+    <div class ="card-body">
 <h1><a type=button href="index.php?page=tasks&action=create">create task</a></h1>
+    </div></div>
 <?php
 include 'footer.php';
 ?>
