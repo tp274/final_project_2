0fdc2a392d94e4aba7354c1a48d5c5154877cb1b
Trupti <tp274@njit.edu>
2017-12-17 13:27:43 -0500

    2 forms

--- pages/login_form.php
+++ pages/login_form.php
@@ -1,17 +1,98 @@
-<html>
-<body  >
-<div class="container">
+<div class="top-content">
 
-    <form class="form-signin" action="index.php?page=accounts&action=login" method="post">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" name = "email" id="email" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" name ="password" id="password" class="form-control" placeholder="Password" required>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <h1><a href="index.php?page=accounts&action=register">New Here? Join Us Now</a></h1>
-    </form>
+    <div class="inner-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-5">
 
-</div> <!-- /container -->
-</body>
-</html>
+                    <div class="form-box">
+                        <div class="form-top">
+                            <div class="form-top-left">
+                                <h3>Login </h3>
+                                <p>Enter username and password to log on:</p>
+                            </div>
+                            <div class="form-top-right">
+                                <i class="fa fa-lock"></i>
+                            </div>
+                        </div>
+                        <div class="form-bottom">
+                            <form role="form" action="index.php?page=accounts&action=login" method="POST" class="login-form">
+                                <div class="form-group">
+                                    <label class="sr-only" for="form-username">Email</label>
+                                    <input type="text" name="email" placeholder="email..." class="form-username form-control" id="form-username">
+                                </div>
+                                <div class="form-group">
+                                    <label class="sr-only" for="form-password">Password</label>
+                                    <input type="password" name="password" placeholder="Password..." class="form-password form-control" id="form-password">
+                                </div>
+                                <button type="submit" class="btn">Sign in</button>
+                            </form>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+    <div class="col-sm-1 middle-border"></div>
+    <div class="col-sm-1"></div>
+
+    <div class="col-sm-5">
+
+        <div class="form-box">
+            <div class="form-top">
+                <div class="form-top-left">
+                    <h3>New here ? Join us now</h3>
+                </div>
+                <div class="form-top-right">
+                    <i class="fa fa-pencil"></i>
+                </div>
+            </div>
+            <div class="form-bottom">
+                <form role="form" action="index.php?page=accounts&action=register" method="POST" class="registration-form">
+                    <div class="form-group">
+                        <label class="sr-only" for="form-first-name">First name</label>
+                        <input type="text" name="fname" placeholder="First name..." class="form-first-name form-control" id="form-first-name">
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="form-last-name">Last name</label>
+                        <input type="text" name="lname" placeholder="Last name..." class="form-last-name form-control" id="form-last-name">
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="form-email">Email</label>
+                        <input type="text" name="email" placeholder="Email..." class="form-email form-control" id="form-email">
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="form-email">Birthday</label>
+                        <input type="date" name="birthday" placeholder="Birthday..." class="form-birthday form-control" id="form-email">
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="form-email">Phone number</label>
+                        <input type="text" name="phone" placeholder="Phone number..." class="form-phonenumber form-control" id="form-email">
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="form-email">Gender</label>
+                        Gender:
+                        <input type="radio" name="gender" value="male" >Male
+                        <input type="radio" name="gender" value="female" >Female
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="form-email">Password</label>
+                        <input type="password" name="password" placeholder="Password..." class="form-password form-control" id="form-email">
+                    </div>
+
+                    <button type="submit" class="btn">Sign up</button>
+                </form>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+</div>
+</div>
+
+</div>
+
+
+</div>
